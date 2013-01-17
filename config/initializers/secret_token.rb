aee3253acf4e52b9fc10b2ce25e80f1acc4da256
Ilya Sokolov <falconmain@gmail.com>
2013-01-17 21:38:12 +0400

    add Rails 3.2.11 boilerplate ёпта

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialPremia::Application.config.secret_token = '95e288c2f6a63ad30ef41cb4cb911dd81fb76bc912707fc0d75d7b4a03d4e776a3e19207afc0071c302f3dbd4298a1c7e267ce91f70542653c95ae2cb18d0e2b'
