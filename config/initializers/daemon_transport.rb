d03b3a43f3a50d0952dd4ccabb33d9031bbd7228
Ilya Sokolov <falconmain@gmail.com>
2013-02-25 21:52:12 +0400

    some transport changes

--- config/initializers/daemon_transport.rb
+++ config/initializers/daemon_transport.rb
@@ -1,15 +1,17 @@
 #MUST IMPLEMENT SEND METHOD
 class DaemonTransport 
   def initialize
-    context = ZMQ::Context.new 1
-    puts "Opening ZMQ connection"
-
-    @outbound = context.socket ZMQ::DOWNSTREAM
-    @outbound.connect "tcp://127.0.0.1:9000"
-    
+    puts "Opening ZMQ connection"    
+    @context = ZMQ::Context.new 1    
   end
-  def send(data)
-    @outbound.send data
+  def send(addr, data)
+    outbound = @context.socket ZMQ::DOWNSTREAM
+    outbound.connect "IPC://tmp/"
+    result = outbound.send data
+    outbound.close
+    result
+  rescue => e
+    puts "Error #{e.inspect}"
   end
 end
 
